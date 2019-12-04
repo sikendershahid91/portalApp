473e2f85add6d9c7b777ffc07a553c3c7b794940
sikender shahid <sikendershahid91@gmail.com>
2019-12-04 07:41:22 -0800

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
-const users = require('./routes/api/users');
+
 
 const app = express();
 
@@ -12,12 +12,6 @@ const app = express();
 app.use(bodyParser.json());
 
 const db = require('./config/keys').mongoURI;
-<<<<<<< HEAD
-
-mongoose.connect(db,{ useNewUrlParser: true})
-    .then(() => console.log('MongoDB successfully connected!'))
-    .catch(err => console.log(err));
-=======
 mongoose.connect(db,
 		 {
 		     useNewUrlParser: true,
@@ -27,13 +21,10 @@ mongoose.connect(db,
 
 //routes
 app.use('/api/users', require('./routes/api/users'));
->>>>>>> new_start
 
 app.use(passport.initialize());
-
 require('./config/passport')(passport);
 
-app.use('/api/users', users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, ()=> console.log(`Server up and running on port ${port} !`));
