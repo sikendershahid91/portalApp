39becd6d9affd220dd1dc1ecb8a696383b7765dd
sikender shahid <sikendershahid91@gmail.com>
2019-12-04 05:39:38 -0800

    update

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://alexisbustos:rodriguez@cluster0-xlno4.mongodb.net/test?retryWrites=true&w=majority'
-};
--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const config = require('config'); 
+
 
 const app = express();
 
-app.use(bodyParser.urlencoded({
-    extended: false
-})
-);
+
 
 app.use(bodyParser.json());
 
-const db = require("./config/keys").mongoURI;
+mongoose.connect(config.get('mongoURI'),
+		 {
+		     useNewUrlParser: true,
+		     useCreateIndex: true
+		 }).then(() => console.log("MongoDB successfully connected!"))
+                 .catch(err => console.log(err));
 
-mongoose.connect(db,{ useNewUrlParser: true})
-    .then(() => console.log("MongoDB successfully connected!"))
-    .catch(err => console.log(err));
-
+//routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const port = process.env.PORT || 5000;
 app.listen(port, ()=> console.log(`Server up and running on port ${port} !`));
