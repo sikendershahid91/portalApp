cfca7eb5a0d30211280fda87218a29fc8d87f029
sikender shahid <sikendershahid91@gmail.com>
2019-12-04 07:32:31 -0800

    update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(db,
 
 //routes
 app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, ()=> console.log(`Server up and running on port ${port} !`));
