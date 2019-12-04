e6ad29ca4435c4fe4c8a79589a99380af5e7182d
sikender shahid <sikendershahid91@gmail.com>
2019-12-04 04:44:29 -0800

    added login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import LoginPage from "./components/loginPage"; 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-	  </header>
-	  <body>
-	  <h1> hello fucking world</h1>
-	  </body>
-	  
-    </div>
+    return (
+	// <div className="App">	
+	    <LoginPage />
+	    // <header className="App-header">
+            // <img src={logo} className="App-logo" alt="logo" />
+	    // </header>
+	    // <body>
+	    // <h1> hello fucking world </h1>
+	    // </body>
+	    
+	// </div>
   );
 }
 
