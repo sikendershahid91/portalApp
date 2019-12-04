8919df7dfcaa6ab50b609c72ccb5a90728d39a3e
sikender shahid <sikendershahid91@gmail.com>
2019-12-04 03:54:40 -0800

    initial fixes on client

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <img src={logo} className="App-logo" alt="logo" />
+	  </header>
+	  <body>
+	  <h1> hello fucking world</h1>
+	  </body>
+	  
     </div>
   );
 }
